13f40591da14e9e28264d518945bb9a72a7802d7
tarekchaalan <tchaalan23@icloud.com>
2024-05-12 06:46:45 -0700

    Removed unneccessary settings

--- project/project/settings.py
+++ project/project/settings.py
@@ -20,7 +20,6 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
     'django.contrib.staticfiles',
     'ForecastApp'
 ]
@@ -33,7 +32,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'project.urls'

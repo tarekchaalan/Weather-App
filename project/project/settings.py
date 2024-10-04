d5d23cfa82daffca0b9825f36559af2a600be0b0
tarekchaalan <tchaalan23@icloud.com>
2024-10-04 18:30:31 +0300

    Removed database

--- project/project/settings.py
+++ project/project/settings.py
@@ -16,11 +16,6 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = ['weather-app-three-mu-24.vercel.app', '.vercel.app', '.now.sh', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
     'django.contrib.staticfiles',
     'ForecastApp'
 ]

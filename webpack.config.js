3ee55cb78160e57e878c48254179ebb111ce1741
tarekchaalan <tchaalan23@icloud.com>
2024-11-02 12:59:55 +0300

    Modifying for vercel compatibility

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,15 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: {
     search: "./src/js/search.js",
     results: "./src/js/results.js",
-    common: "./src/js/common.js",
   },
   output: {
-    filename: "[name].bundle.js",
+    filename: "js/[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
   },
@@ -18,20 +18,24 @@ module.exports = {
   devtool: "source-map",
   module: {
     rules: [
+      // JavaScript
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
           loader: "babel-loader",
           options: {
+            sourceType: "unambiguous",
             presets: ["@babel/preset-env"],
           },
         },
       },
+      // CSS
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
       },
+      // Assets
       {
         test: /\.(png|jpg|jpeg|gif|ico|svg|mp4)$/i,
         type: "asset/resource",
@@ -43,15 +47,18 @@ module.exports = {
   },
   plugins: [
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "css/[name].[contenthash].css",
+    }),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
-      chunks: ["common", "search"],
+      chunks: ["search"],
     }),
     new HtmlWebpackPlugin({
       template: "./src/results.html",
       filename: "results.html",
-      chunks: ["common", "results"],
+      chunks: ["results"],
     }),
     new CopyWebpackPlugin({
       patterns: [
